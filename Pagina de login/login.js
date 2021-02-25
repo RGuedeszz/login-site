b82ecca823c03e63647a57c05283207c623c4d1d
rojao <51566699+rojao@users.noreply.github.com>
2021-02-24 23:31:03 -0300

    Add files via upload

---
+++ Pagina de login/login.js
@@ -0,0 +1,64 @@
+var login;
+var senha;
+function dados() {
+    login = document.getElementById("login").value;
+    senha = document.getElementById("senha").value;
+
+    document.write("Login: " + login + "---. Senha: " + senha);
+}
+
+function confirmaEnter(tecla) {
+    if (tecla.keyCode == 13) { // Enter
+        login = document.getElementById("login").value;
+        senha = document.getElementById("senha").value;
+
+        document.write("Login: " + login + "---. Senha: " + senha);
+    }
+}
+
+/*
+function trocaVisibilidade() {
+    let olho = document.getElementById("visibilidade");
+
+    if (olho.value == "protegido") {
+        olho.value = "mostrando";
+        document.getElementById("senha").DOCUMENT_TYPE_NODE
+    } else {
+        olho.value = "protegido";
+    }
+    window.alert(olho.value);
+}
+*/
+
+function confirmaCadastro() {
+    let podeConfirmar = false;
+    let completo = false;
+    let senhaIgual = false;
+    let nomeCadastro = document.getElementById("nomeCad").value;
+    let nascimentoCadastro = document.getElementById("dataCad").value;
+    let emailCadastro = document.getElementById("emailCad").value;
+    let senhaCadastro = document.getElementById("senhaCad").value;
+    let confirmaSenhaCadastro = document.getElementById("confirmaSenhaCad").value;
+    let confirmaTelefone = document.getElementById("telefoneCad").value;
+    
+
+    if ((nomeCadastro && nascimentoCadastro && emailCadastro && senhaCadastro && confirmaSenhaCadastro && confirmaTelefone) != 0) {
+        completo = true;
+    } else {
+        window.alert("Faltam dados");
+    }
+    if (senhaCadastro == confirmaSenhaCadastro) {
+        senhaIgual = true;
+    } else if (completo) {
+        window.alert("As senhas nao batem");
+    }
+
+    if (senhaIgual && completo) {
+        podeConfirmar = true;
+    }
+
+    if (podeConfirmar) {
+        window.alert("Tudo certo");
+    }
+    
+}
